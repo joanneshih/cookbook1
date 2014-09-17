d3feb06890c86d1a6e1c91191b4e56a89292910c
Joanne <joanneshih@joanneshih-NB2.msi.com.tw>
2014-09-17 17:12:50 +0800

    Signed-off-by: Joanne <joanneshih@joanneshih-NB2.msi.com.tw>

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "sharedwritable"
+maintainer       "MSI"
+maintainer_email "joanneshih@msi.com"
+license          "All rights reserved"
+description      "Installs/Configures shared log" 
+version          "0.0.1"
+supports 'ubuntu'
+recipe "sharedwritable::default", "Only one recipe"
